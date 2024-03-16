0be22a09c3a9db422d8f34d41a5251a62b5b11cf
Geordi Van Nieuland <vannieuland.geordi@gmail.com>
2024-03-16 06:21:06 +0100

    Change order based on screen size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,13 +10,13 @@ export default function Home() {
     <main>
       <Section>
         <div className="grid grid-cols-12 gap-3 grid-flow-row">
-          <div className="col-span-8">
+          <div className="col-span-12  md:col-span-8  order-3 md:order-1">
             <CsvSection />
           </div>
-          <div className="col-span-4">
+          <div className=" col-span-12  md:col-span-4 order-2">
             <FilterSection />
           </div>
-          <div className="col-span-12">
+          <div className="col-span-12 order-1 md:order-3">
           <ChartSection />
           </div>
         </div>
